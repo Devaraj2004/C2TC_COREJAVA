1ebbd72aba42a4b46729f5a70f11d9dcee2539a5
Admin <Admin@DESKTOP-JED55LM>
2024-10-25 02:19:46 -0700

    identifier

---
+++ C2TC_DEVARAJM/src/day1/day2.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class day2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		int num=07;
+		System.out.println("value of the number variable is :"+num);
+		String Name="Devaraj";
+		System.out.println("value is:"+Name);
+
+	}
+
+}
