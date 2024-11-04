81634d1ca2ee1154c23a195c21ed62afa3578d09
Admin <Admin@DESKTOP-JED55LM>
2024-11-03 16:10:38 -0800

    day3

---
+++ C2TC_DEVARAJM/src/day3encap/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package day3encap;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) { 
+		//object creation  
+		OopsConceptDemo obj = new 
+		OopsConceptDemo(); 
+		obj.setSerialNum(101); 
+		obj.setName("dev"); 
+		obj.setAge(20); 
+		System.out.println(obj); 
+		} 
+
+}
---
+++ C2TC_DEVARAJM/src/day3inherit/SLInheritanceDemo.java
@@ -0,0 +1,8 @@
+package day3inherit;
+
+public class SLInheritanceDemo {
+	public static void main(String[] args) { 
+		Student student = new Student("dev", "8867864780", "Chennai", 9888877L, 12, "City College"); 
+		System.out.println(student); 
+		}
+}
